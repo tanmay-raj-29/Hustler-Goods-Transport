071c6db964a2748054ba63c8dc9b12feac20d457
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-02-12 14:03:15 +0530

    add route, dealer, driver serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,31 @@
 from rest_framework import serializers
-from .models import City
+from .models import City, Route, Dealer, Driver
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
+        fields = '__all__'
+
+
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = '__all__'
+
+
+
+
+class DealerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dealer
+        fields = '__all__'
+
+
+
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
         fields = '__all__'
