3df80a8fa432235b0fbf31c35c8c920d5dae8fa4
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-02-12 12:02:20 +0530

    add city model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    state = models.CharField(max_length=150, blank=False)
+    latitude = models.DecimalField(decimal_places=8)
+    longitude = models.DecimalField(decimal_places=8)
+
+    def __str__(self):
+        return self.name + '|' + self.state
