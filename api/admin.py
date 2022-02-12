8ed6986f11e7f14bbd974c48caecd03537134a2b
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-02-12 14:06:32 +0530

    register route, dealer, driver model

--- api/admin.py
+++ api/admin.py
@@ -3,3 +3,6 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.City)
+admin.site.register(models.Route)
+admin.site.register(models.Dealer)
+admin.site.register(models.Driver)
