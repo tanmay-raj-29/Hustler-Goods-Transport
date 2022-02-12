01381ff01da6c2dc6376c25d883b898bb1465554
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-02-12 14:02:59 +0530

    add route, dealer, driver model

--- api/models.py
+++ api/models.py
@@ -4,8 +4,60 @@ from django.db import models
 class City(models.Model):
     name = models.CharField(max_length=150, blank=False)
     state = models.CharField(max_length=150, blank=False)
-    latitude = models.DecimalField(decimal_places=8)
-    longitude = models.DecimalField(decimal_places=8)
+    latitude = models.DecimalField(decimal_places=8, max_digits=15)
+    longitude = models.DecimalField(decimal_places=8, max_digits=15)
 
     def __str__(self):
         return self.name + '|' + self.state
+
+
+
+
+class Route(models.Model):
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city')
+    to_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='to_city')
+    days = models.PositiveIntegerField(blank=False)
+
+    def __str__(self):
+        return self.from_city + ' -> ' +self.to_city
+
+
+
+
+class Dealer(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    mobile_number = models.CharField(max_length=10, blank=False)
+    material_nature = models.CharField(max_length=100, blank=False)
+    material_weight = models.CharField(max_length=100, blank=False)
+    quantity = models.IntegerField(blank=False)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Driver(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    age = models.PositiveIntegerField(blank=False)
+    truck_number = models.CharField(max_length=100, blank=False)
+    mobile_number = models.CharField(max_length=10, blank=False)
+    capacity = models.PositiveIntegerField(blank=False)
+    transporter_name = models.CharField(max_length=150, blank=False)
+    experience = models.PositiveIntegerField(blank=False)
+    routes = models.ManyToManyField(Route)
+    def __str__(self):
+        return self.name
+
+
+
+
+# class Deal(models.Model):
+#     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+#     dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE)
+#     route = models.ForeignKey(Route, on_delete=models.CASCADE)
+#     is_confirmed = models.BooleanField()
+
+#     def __str__(self):
+#         return self.driver + ' & ' + self.dealer
