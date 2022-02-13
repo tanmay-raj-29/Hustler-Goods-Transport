30f1b395ae1be95d6a2c742ddfe53c3bc326ac71
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-02-13 09:46:49 +0530

    add views for dealers, routes, drivers, cities

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,14 @@ from . import views
 
 urlpatterns = [
     path('cities/', views.cities, name='cities'),
+    path('cities/<int:pk>/dealers', views.city_dealers, name='city_dealers'),
+
     path('routes/', views.routes, name='routes'),
+    path('routes/<int:pk>/drivers/', views.route_drivers, name='route_drivers'),
+    
     path('dealers/', views.dealers, name='dealers'),
+    path('dealers/<int:pk>/', views.dealer, name='dealer'),
+    
     path('drivers/', views.drivers, name='drivers'),
+    path('drivers/<int:pk>/', views.driver, name='driver'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import City, Route, Dealer, Driver
 from .serializers import CitySerializer, RouteSerializer, DealerSerializer, DriverSerializer
+from django.http import HttpResponse
 
 # Create your views here.
 @api_view(['GET'])
@@ -12,11 +13,26 @@ def cities(request):
 
 
 @api_view(['GET'])
+def city_dealers(request, pk):
+    city = City.objects.get(pk=pk)
+    dealers = city.dealer_set.all()
+    serializer = DealerSerializer(dealers, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
 def routes(request):
     routes = Route.objects.all()
     serializer = RouteSerializer(routes, many=True)
     return Response(serializer.data)
 
+
+@api_view(['GET'])
+def route_drivers(request, pk):
+    route = Route.objects.get(pk=pk)
+    drivers = route.driver_set.all()
+    serializer = DriverSerializer(drivers, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -26,9 +42,28 @@ def dealers(request):
     return Response(serializer.data)
 
 
+@api_view(['GET'])
+def dealer(request, pk):
+    try:
+        dealer = Dealer.objects.get(pk=pk)
+    except Dealer.DoesNotExist:
+        return HttpResponse(status=404)
+    serializer = DealerSerializer(dealer)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def drivers(request):
     drivers = Driver.objects.all()
     serializer = DriverSerializer(drivers, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def driver(request, pk):
+    try:
+        driver = Driver.objects.get(pk=pk)
+    except Driver.DoesNotExist:
+        return HttpResponse(status=404)
+    serializer = DriverSerializer(driver)
     return Response(serializer.data)
